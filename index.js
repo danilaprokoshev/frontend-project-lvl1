bce14ded93e4664278369ebfcb7aea194346f091
Danila Prokoshev <danilaprokoshev@gmail.com>
2020-11-05 22:51:24 +0300

    updated index.js on step 6

--- index.js
+++ index.js
@@ -21,3 +21,37 @@ export const brainEven = async () => {
 
   return console.log(`Congratulations, ${name}!`);
 };
+
+export const brainCalc = async () => {
+  const name = await promptly.prompt('May I have your name: ');
+  console.log(`Hello, ${name}!`);
+  console.log('What is the result of the expression?');
+  const operators = ['+', '-', '*'];
+  let countRightAnswers = 0;
+  while (countRightAnswers < 3) {
+    const firstOperand = Math.round(Math.random() * 100);
+    const secondOperand = Math.round(Math.random() * 100);
+    const opIndex = Math.floor(Math.random() * 3);
+    let correctAnswer = 0;
+    switch (operators[opIndex]) {
+      case '+':
+        correctAnswer = firstOperand + secondOperand;
+        break;
+      case '-':
+        correctAnswer = firstOperand - secondOperand;
+        break;
+      case '*':
+        correctAnswer = firstOperand * secondOperand;
+        break;
+      default:
+    }
+    console.log(`Question: ${firstOperand} ${operators[opIndex]} ${secondOperand}`);
+    const answer = await promptly.prompt('Your answer: ');
+    if (Number(answer) === correctAnswer) {
+      console.log('Correct!');
+      countRightAnswers += 1;
+    } else return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`, '\n', `Let's try again, ${name}!`);
+  }
+
+  return console.log(`Congratulations, ${name}!`);
+};
