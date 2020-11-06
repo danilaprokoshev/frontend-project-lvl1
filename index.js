d4359d1af17dd3c39f5449b9e4d99452268ffab1
Danila Prokoshev <danilaprokoshev@gmail.com>
2020-11-06 22:07:30 +0300

    updated index.js on step 9

--- index.js
+++ index.js
@@ -109,3 +109,33 @@ export const brainProgression = async () => {
 
   return console.log(`Congratulations, ${name}!`);
 };
+
+export const brainPrime = async () => {
+  function isPrime(number) {
+    for (let i = 2; i < number / 2; i += 1) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  const name = await promptly.prompt('May I have your name: ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  let countRightAnswers = 0;
+  while (countRightAnswers < 3) {
+    let correctAnswer = '';
+    const arbitraryNumber = Math.round(Math.random() * (3571 - 2) + 2);
+    if (isPrime(arbitraryNumber)) {
+      correctAnswer = 'yes';
+    } else correctAnswer = 'no';
+    console.log(`Question: ${arbitraryNumber}`);
+    const answer = await promptly.prompt('Your answer: ');
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      countRightAnswers += 1;
+    } else return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`, '\n', `Let's try again, ${name}!`);
+  }
+
+  return console.log(`Congratulations, ${name}!`);
+};
