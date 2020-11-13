eeb303eb5b456bb77637c5f167c29f530c7c0608
Danila Prokoshev <danilaprokoshev@gmail.com>
2020-11-13 17:22:09 +0300

    updated src/index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-export default (game) => game();
+import promptly from 'promptly';
+
+export default async (game) => {
+  const name = await promptly.prompt('May I have your name: ');
+  console.log(`Hello, ${name}!`);
+  console.log(game.GREETING);
+  for (let i = 0; i < 3; i += 1) {
+    const question = game.getQuestion();
+    const correctAnswer = game.getCorrectAnswer(question);
+    console.log(`Question: ${question}`);
+    const answer = await promptly.prompt('Your answer: ');
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`, '\n', `Let's try again, ${name}!`);
+    }
+  }
+
+  return console.log(`Congratulations, ${name}!`);
+};
