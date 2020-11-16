984739bfda52b6e339cf7bcd0e3a2178c6ba3dfa
Danila Prokoshev <danilaprokoshev@gmail.com>
2020-11-16 19:01:41 +0300

    updated bin/brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import quizGame from '../src/index.js';
-import brainProgression from '../games/brain-progression.js';
+import * as brainProgression from '../games/brain-progression.js';
 
 quizGame(brainProgression);
