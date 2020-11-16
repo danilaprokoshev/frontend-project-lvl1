b3e410b344c4cce9ab7028505d5d564d5d20c9fb
Danila Prokoshev <danilaprokoshev@gmail.com>
2020-11-16 19:03:08 +0300

    updated games/brain-gcd.js

--- games/brain-gcd.js
+++ games/brain-gcd.js
@@ -1,26 +1,24 @@
-import promptly from 'promptly';
+const GREETING = 'Find the greatest common divisor of given numbers.';
 
-export default async () => {
-  const name = await promptly.prompt('May I have your name: ');
-  console.log(`Hello, ${name}!`);
-  console.log('Find the greatest common divisor of given numbers.');
-  let countRightAnswers = 0;
-  while (countRightAnswers < 3) {
-    const firstNumber = Math.round(Math.random() * 100);
-    const secondNumber = Math.round(Math.random() * 100);
-    let gcd = 1;
-    for (let divisor = 2; divisor <= Math.max(firstNumber, secondNumber); divisor += 1) {
-      if (firstNumber % divisor === 0 && secondNumber % divisor === 0) {
-        gcd = divisor;
-      }
+const getQuestion = () => {
+  const firstNumber = Math.round(Math.random() * 100);
+  const secondNumber = Math.round(Math.random() * 100);
+
+  return `${firstNumber} ${secondNumber}`;
+};
+
+const getCorrectAnswer = (question) => {
+  const [firstNumber, secondNumber] = question.split(' ');
+  const firstNumberAsNum = Number(firstNumber);
+  const secondNumberAsNum = Number(secondNumber);
+  let gcd = 1;
+  for (let divisor = 2; divisor <= Math.max(firstNumberAsNum, secondNumberAsNum); divisor += 1) {
+    if (firstNumberAsNum % divisor === 0 && secondNumberAsNum % divisor === 0) {
+      gcd = divisor;
     }
-    console.log(`Question: ${firstNumber} ${secondNumber}`);
-    const answer = await promptly.prompt('Your answer: ');
-    if (Number(answer) === gcd) {
-      console.log('Correct!');
-      countRightAnswers += 1;
-    } else return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${gcd}'.`, '\n', `Let's try again, ${name}!`);
   }
 
-  return console.log(`Congratulations, ${name}!`);
+  return String(gcd);
 };
+
+export { GREETING, getQuestion, getCorrectAnswer };
