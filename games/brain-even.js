23e328a1cd8df0d13732728f550846492031a802
Danila Prokoshev <danilaprokoshev@gmail.com>
2020-11-16 19:02:41 +0300

    updated games/brain-even.js

--- games/brain-even.js
+++ games/brain-even.js
@@ -1,10 +1,6 @@
 const GREETING = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const getQuestion = () => {
-  const question = Math.round(Math.random() * 100);
-
-  return question;
-};
+const getQuestion = () => Math.round(Math.random() * 100);
 
 const getCorrectAnswer = (question) => (question % 2 === 0 ? 'yes' : 'no');
 
