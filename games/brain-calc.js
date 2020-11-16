30d332af07c4268495ba75a356ed26b1dd9bbfc1
Danila Prokoshev <danilaprokoshev@gmail.com>
2020-11-16 19:02:24 +0300

    updated games/brain-calc.js

--- games/brain-calc.js
+++ games/brain-calc.js
@@ -1,35 +1,33 @@
-import promptly from 'promptly';
+const GREETING = 'What is the result of the expression?';
 
-export default async () => {
-  const name = await promptly.prompt('May I have your name: ');
-  console.log(`Hello, ${name}!`);
-  console.log('What is the result of the expression?');
+const getQuestion = () => {
   const operators = ['+', '-', '*'];
-  let countRightAnswers = 0;
-  while (countRightAnswers < 3) {
-    const firstOperand = Math.round(Math.random() * 100);
-    const secondOperand = Math.round(Math.random() * 100);
-    const opIndex = Math.floor(Math.random() * 3);
-    let correctAnswer = 0;
-    switch (operators[opIndex]) {
-      case '+':
-        correctAnswer = firstOperand + secondOperand;
-        break;
-      case '-':
-        correctAnswer = firstOperand - secondOperand;
-        break;
-      case '*':
-        correctAnswer = firstOperand * secondOperand;
-        break;
-      default:
-    }
-    console.log(`Question: ${firstOperand} ${operators[opIndex]} ${secondOperand}`);
-    const answer = await promptly.prompt('Your answer: ');
-    if (Number(answer) === correctAnswer) {
-      console.log('Correct!');
-      countRightAnswers += 1;
-    } else return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`, '\n', `Let's try again, ${name}!`);
+  const firstOperand = Math.round(Math.random() * 100);
+  const secondOperand = Math.round(Math.random() * 100);
+  const opIndex = Math.floor(Math.random() * 3);
+
+  return `${firstOperand} ${operators[opIndex]} ${secondOperand}`;
+};
+
+const getCorrectAnswer = (question) => {
+  let correctAnswer = 0;
+  const [firstOperand, operator, secondOperand] = question.split(' ');
+  const firstOperandAsNum = Number(firstOperand);
+  const secondOperandAsNum = Number(secondOperand);
+  switch (operator) {
+    case '+':
+      correctAnswer = firstOperandAsNum + secondOperandAsNum;
+      break;
+    case '-':
+      correctAnswer = firstOperandAsNum - secondOperandAsNum;
+      break;
+    case '*':
+      correctAnswer = firstOperandAsNum * secondOperandAsNum;
+      break;
+    default:
   }
 
-  return console.log(`Congratulations, ${name}!`);
+  return String(correctAnswer);
 };
+
+export { GREETING, getQuestion, getCorrectAnswer };
