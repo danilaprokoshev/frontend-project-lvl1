f35bf8e61a94bd9575a9aae2887b8bcad482d4de
Danila Prokoshev <danilaprokoshev@gmail.com>
2020-11-17 23:54:15 +0300

    updated games/brain-even.js

--- games/brain-even.js
+++ games/brain-even.js
@@ -3,7 +3,7 @@ import generateNumber from '../src/generating-numbers.js';
 const START_NUMBER = 0;
 const FINISH_NUMBER = 100;
 
-const greeting = () => 'Answer "yes" if the number is even, otherwise answer "no".';
+const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (number) => number % 2 === 0;
 
@@ -11,4 +11,4 @@ const getQuestion = () => generateNumber(START_NUMBER, FINISH_NUMBER);
 
 const getCorrectAnswer = (question) => (isEven(question) ? 'yes' : 'no');
 
-export { greeting, getQuestion, getCorrectAnswer };
+export { gameDescription, getQuestion, getCorrectAnswer };
