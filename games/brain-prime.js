94720724e448c962788280dd17c1454408992d88
Danila Prokoshev <danilaprokoshev@gmail.com>
2020-11-17 23:25:12 +0300

    updated games/brain-prime.js

--- games/brain-prime.js
+++ games/brain-prime.js
@@ -1,9 +1,12 @@
-const GREETING = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const START_INT = 2;
-const FINISH_INT = 3571;
+import generateNumber from '../src/generating-numbers.js';
+
+const START_NUMBER = 2;
+const FINISH_NUMBER = 3571;
+
+const greeting = () => 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 function isPrime(number) {
-  for (let i = 2; i < number / 2; i += 1) {
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) {
       return false;
     }
@@ -11,15 +14,8 @@ function isPrime(number) {
   return true;
 }
 
-const getQuestion = () => Math.round(Math.random() * (FINISH_INT - START_INT) + START_INT);
+const getQuestion = () => generateNumber(START_NUMBER, FINISH_NUMBER);
 
-const getCorrectAnswer = (question) => {
-  let correctAnswer = '';
-  if (isPrime(question)) {
-    correctAnswer = 'yes';
-  } else correctAnswer = 'no';
+const getCorrectAnswer = (question) => (isPrime(question) ? 'yes' : 'no');
 
-  return correctAnswer;
-};
-
-export { GREETING, getQuestion, getCorrectAnswer };
+export { greeting, getQuestion, getCorrectAnswer };
