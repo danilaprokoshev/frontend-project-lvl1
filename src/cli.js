164784dedb3fd8a99b2476c19f163094f789c094
Danila Prokoshev <danilaprokoshev@gmail.com>
2020-11-26 09:32:24 +0300

    updated src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import promptly from 'promptly';
 
 export default async () => {
+  console.log('Welcome to the Brain Games!');
   const name = await promptly.prompt('May I have your name: ');
   console.log(`Hello, ${name}!`);
 };
