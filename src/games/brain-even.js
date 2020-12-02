c33ffdff074475893d76d3bda040e7cde83de6fd
Danila Prokoshev <danilaprokoshev@gmail.com>
2020-12-03 01:39:53 +0300

    updated src/games/brain-even on check#4

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -8,12 +8,11 @@ const gameDescription = 'Answer "yes" if the number is even, otherwise answer "n
 
 const isEven = (number) => number % 2 === 0;
 
-const getQuestion = () => generateNumber(START_NUMBER, FINISH_NUMBER);
+const getQuestionAndAnswer = () => {
+  const question = generateNumber(START_NUMBER, FINISH_NUMBER);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
 
-const getCorrectAnswer = (question) => (isEven(question) ? 'yes' : 'no');
-
-const startGame = () => quizGame(gameDescription, getQuestion, getCorrectAnswer);
-
-export {
-  gameDescription, getQuestion, getCorrectAnswer, startGame,
+  return [question, correctAnswer];
 };
+
+export default () => quizGame(gameDescription, getQuestionAndAnswer);
